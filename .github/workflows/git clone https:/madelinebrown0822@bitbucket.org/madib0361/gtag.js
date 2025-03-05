0b74784a17bb7063af04929ad10b3f795658f3b1
Madeline Nicole Brown <104762490+Madib036@users.noreply.github.com>
2025-03-04 22:32:02 -0600

    Update gtag.js

--- .github/workflows/git clone https:/madelinebrown0822@bitbucket.org/madib0361/gtag.js
+++ .github/workflows/git clone https:/madelinebrown0822@bitbucket.org/madib0361/gtag.js
@@ -1,6 +1,6 @@
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-954PBE94JK"></script>
-<script>
+<Set-ExecutionPolicy Bypass -Scope Process>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
