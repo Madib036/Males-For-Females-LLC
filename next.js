d5ea3cbd4c5ddf611eecc044bf10af0be9b200a8
Madeline Nicole Brown <104762490+Madib036@users.noreply.github.com>
2026-07-20 22:10:44 -0500

    Create next.js

---
+++ next.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <meta name="base:app_id" content="6972dd8e88e3bac59cf3d478" />
+      </Head>
+      {/* Your page content */}
+    </>
+  );
+}
